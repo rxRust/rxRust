5d38ca5141836116ef1d2913c8f2fe1650d29881
adoo <Adoo@outlook.com>
2019-05-17 20:29:37 +0800

    feat: implement map operator

---
+++ src/ops/map.rs
@@ -0,0 +1,72 @@
+use crate::Observable;
+
+pub trait Map<'a, T> {
+  /// Creates a new stream which calls a closure on each element and uses
+  /// its return as the value.
+  ///
+  fn map<B, F>(self, f: F) -> MapOP<Self, F>
+  where
+    Self: Sized,
+    F: FnMut(T) -> B + 'a,
+  {
+    MapOP {
+      stream: self,
+      func: f,
+    }
+  }
+}
+
+impl<'a, T, O> Map<'a, T> for O where O: Observable<'a> {}
+
+
+pub struct MapOP<S, M> {
+  stream: S,
+  func: M,
+}
+
+
+impl<'a, B, S, M> Observable<'a> for MapOP<S, M>
+where
+  S: Observable<'a>,
+  M: FnMut(S::Item) -> B + 'a,
+{
+  type Item = B;
+
+  fn subscribe<O>(self, mut observer: O)
+  where
+    O: 'a + FnMut(Self::Item),
+  {
+    let mut func = self.func;
+    self.stream.subscribe(move |v| {
+      observer(func(v));
+    })
+  }
+}
+
+
+#[cfg(test)]
+mod test {
+  use crate::{ops::Map, Observable, Observer, Subject};
+
+  #[test]
+  fn primtive_type() {
+    let mut i = 0;
+    {
+      let broadcast = Subject::new();
+      broadcast.clone().map(|i| i * 2).subscribe(|v| i = v);
+      broadcast.next(100);
+    }
+    assert_eq!(i, 200);
+  }
+
+  #[test]
+  fn reference_lifetim_should_work() {
+    let mut i = 0;
+    {
+      let broadcast = Subject::new();
+      broadcast.clone().map(|v: &&i32| v).subscribe(|v| i = **v);
+      broadcast.next(&100);
+    }
+    assert_eq!(i, 100);
+  }
+}
