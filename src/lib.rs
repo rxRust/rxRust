5a6bc9f96e511a81524d92f61085b96eb133000a
adoo <Adoo@outlook.com>
2019-05-23 13:36:33 +0800

    refactor: split `on_error` and `on_complete` from `subscribe` method Now use `rx_rs` in this way: ```rust   subject     .map(...)     .filter(...)     .subscribe(|v| { ... })     // if need error handding     .on_error(|err| { ... })     // completed process, if need     .on_complete(|| { ... }); ```

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,14 @@ pub use subject::Subject;
 pub trait Observable<'a>: Sized {
   /// The type of the elements being emitted.
   type Item: Sized;
-  /// The type of error may occur.
+  //
   type Err;
   // the Subscription subsribe method return.
-  type Unsubscribe: Subscription;
+  type Unsubscribe: Subscription<'a, Err = Self::Err>;
 
-  fn subscribe<N, EC>(self, next: N, err_or_complete: EC) -> Self::Unsubscribe
+  fn subscribe<N>(self, next: N) -> Self::Unsubscribe
   where
-    N: 'a + Fn(Self::Item),
-    EC: 'a + Fn(&ErrComplete<Self::Err>);
+    N: 'a + Fn(Self::Item);
 
   fn broadcast(self) -> Subject<'a, Self::Item, Self::Err>
   where
@@ -35,14 +34,16 @@ pub trait Observer {
 
   fn complete(self);
 
-  fn err(self, err: Self::Err);
+  fn error(self, err: Self::Err);
 }
 
-pub trait Subscription {
+pub trait Subscription<'a> {
+  type Err;
+  fn on_error<E>(&mut self, err: E) -> &mut Self
+  where
+    E: Fn(&Self::Err) + 'a;
+  fn on_complete<C>(&mut self, complete: C) -> &mut Self
+  where
+    C: Fn() + 'a;
   fn unsubscribe(self);
-}
-
-pub enum ErrComplete<E> {
-  Complete,
-  Err(E),
 }
--- src/ops/filter.rs
+++ src/ops/filter.rs
@@ -1,21 +1,20 @@
-use crate::{ErrComplete, Observable};
+use crate::Observable;
 
 /// Emit only those items from an Observable that pass a predicate test
 /// # Example
 ///
 /// ```
-/// use rx_rs::{ops::Filter, Subject, Observable, Observer, ErrComplete};
+/// use rx_rs::{ops::Filter, Subject, Observable, Observer};
 /// use std::cell::RefCell;
 /// use std::rc::Rc;
 ///
-/// let subject = Subject::new();
+/// let subject = Subject::<'_, _, ()>::new();
 /// let coll = Rc::new(RefCell::new(vec![]));
 /// let coll_clone = coll.clone();
 ///
 /// subject.clone().filter(|v| *v % 2 == 0).subscribe(move |v| {
 ///    coll_clone.borrow_mut().push(*v);
-/// },
-/// |ec: &ErrComplete<()>| {});
+/// });
 
 /// (0..10).into_iter().for_each(|v| {
 ///    subject.next(v);
@@ -50,23 +49,19 @@ where
   S: Observable<'a>,
   F: 'a + Fn(&S::Item) -> bool,
 {
+  type Err = S::Err;
   type Item = S::Item;
   type Unsubscribe = S::Unsubscribe;
-  type Err = S::Err;
 
-  fn subscribe<N, EC>(self, next: N, err_or_complete: EC) -> Self::Unsubscribe
+  fn subscribe<N>(self, next: N) -> Self::Unsubscribe
   where
     N: 'a + Fn(Self::Item),
-    EC: 'a + Fn(&ErrComplete<Self::Err>),
   {
     let filter = self.filter;
-    self.source.subscribe(
-      move |v| {
-        if filter(&v) {
-          next(v);
-        }
-      },
-      err_or_complete,
-    )
+    self.source.subscribe(move |v| {
+      if filter(&v) {
+        next(v);
+      }
+    })
   }
 }
