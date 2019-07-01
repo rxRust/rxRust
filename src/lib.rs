8467a4e3be438907a2565b8eb1ddc0695292a053
adoo <Adoo@outlook.com>
2019-07-01 23:05:35 +0800

    feat: add observable::from_iter

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,16 @@ pub mod subscriber;
 pub mod subscription;
 
 pub mod prelude {
+  pub use crate::observable;
   pub use crate::observable::Observable;
+  pub use crate::ops;
+  pub use crate::subject;
   pub use crate::subject::Subject;
+  pub use crate::subscribable;
   pub use crate::subscribable::*;
+  pub use crate::subscriber;
   pub use crate::subscriber::Subscriber;
+  pub use crate::subscription;
   pub use crate::subscription::*;
   pub use crate::Observer;
 }
---
+++ src/observable/from_iter.rs
@@ -0,0 +1,38 @@
+use crate::prelude::*;
+
+pub fn from_iter<'a, T: 'a, Item: 'a, Err: 'a>(
+  iter: T,
+) -> impl Subscribable<'a, Item = Item, Err = Err>
+where
+  T: IntoIterator<Item = Item>,
+{
+  Observable::new(move |subcriber| {
+    iter
+      .into_iter()
+      .take_while(|_| !subcriber.is_stopped())
+      .for_each(|v| {
+        subcriber.next(&v);
+      });
+    if !subcriber.is_stopped() {
+      subcriber.complete();
+    }
+  })
+}
+
+#[cfg(test)]
+mod test {
+  use crate::prelude::*;
+  use std::cell::Cell;
+
+  #[test]
+  fn iter_to_observable() {
+    let hit_count = Cell::new(0);
+    let completed = Cell::new(false);
+    observable::from_iter::<'_, _, _, ()>(0..100)
+      .subscribe(|_| hit_count.set(hit_count.get() + 1))
+      .on_complete(|| completed.set(true));
+
+    assert_eq!(hit_count.get(), 100);
+    assert_eq!(completed.get(), true);
+  }
+}
