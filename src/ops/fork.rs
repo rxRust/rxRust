11c81406f1cd9827ab0f37101a8168174d63fc35
adoo <Adoo@outlook.com>
2019-07-14 22:08:17 +0800

    feat: let filter extension support fork

--- src/ops/fork.rs
+++ src/ops/fork.rs
@@ -3,25 +3,25 @@
 /// # Example
 /// ```rust ignore
 ///  # use rx_rs::prelude::*;
-///  let o = observable::from_range(0..10);
+///  let o = observable::from_iter(0..10);
 ///  o.subscribe_err(|_| {println!("consume in first")}, |_:&()|{});
 ///  o.subscribe_err(|_| {println!("consume in second")}, |_:&()|{});
 /// ```
 /// it will compile failed, complains like this:
 /// ```
-// 5 |  let o = observable::from_range(0..10);
-//   |      - move occurs because `o` has type `rx_rs::observable::from_iter::RangeObservable<'_, i32, ()>`, which does not implement the `Copy` trait
+// 5 |  let o = observable::from_iter(0..10);
+//   |      - move occurs because `o` has type `rx_rs::observable::from_iter::RangeObservable`, which does not implement the `Copy` trait
 // 6 |  o.subscribe_err(|_| {println!("consume in first")}, |_:&()|{});
 //   |  - value moved here
 // 7 |  o.subscribe_err(|_| {println!("consume in second")}, |_:&()|{});
 //   |  ^ value used here after move
 /// ```
-/// 
+
 /// use `fork` to resolve it
 /// ```rust
 ///  # use rx_rs::prelude::*;
 ///  # use rx_rs::ops::Fork;
-///  let o = observable::from_range(0..10);
+///  let o = observable::from_iter(0..10);
 ///  o.fork().subscribe_err(|_| {println!("consume in first")}, |_:&()|{});
 ///  o.fork().subscribe_err(|_| {println!("consume in second")}, |_:&()|{});
 /// ```
@@ -48,6 +48,25 @@ where
   type Item = Item;
   type Err = Err;
   type Unsub = S::Unsub;
+
+  #[inline]
+  fn subscribe_return_state(
+    self,
+    next: impl Fn(&Self::Item) -> OState<Self::Err> + 'a,
+    error: Option<impl Fn(&Self::Err) + 'a>,
+    complete: Option<impl Fn() + 'a>,
+  ) -> Self::Unsub {
+    self.0.subscribe_return_state(next, error, complete)
+  }
+}
+
+impl<'a, Item, Err: 'a, S> ImplSubscribable<'a> for &'a Sink<&S>
+where
+  &'a S: ImplSubscribable<'a, Item = Item, Err = Err>,
+{
+  type Item = Item;
+  type Err = Err;
+  type Unsub = <&'a S as ImplSubscribable<'a>>::Unsub;
 
   #[inline]
   fn subscribe_return_state(
