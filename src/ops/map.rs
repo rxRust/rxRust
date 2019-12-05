8ce12b65ee1d008cf7a211668de521a250817614
adoo <Adoo@outlook.com>
2019-12-05 20:56:47 +0800

    refactor: refactor `map` and `filter_map` to have a simple and clear interface

--- src/ops/map.rs
+++ src/ops/map.rs
@@ -16,20 +16,6 @@ pub trait Map<T> {
       _p: PhantomData,
     }
   }
-
-  /// A version of map extension which return reference, and furthermore，return
-  /// type and input item has same lifetime.
-  fn map_return_ref<B, F>(self, f: F) -> MapReturnRefOp<Self, F, B>
-  where
-    Self: Sized,
-    F: Fn(B) -> T,
-  {
-    MapReturnRefOp {
-      source: self,
-      func: f,
-      _p: PhantomData,
-    }
-  }
 }
 
 impl<O, Item> Map<Item> for O {}
@@ -123,95 +109,6 @@ where
   }
 }
 
-pub struct MapReturnRefOp<S, M, B> {
-  source: S,
-  func: M,
-  _p: PhantomData<B>,
-}
-
-impl<Item, Err, O, U, S, B, M> RawSubscribable<Item, Err, Subscriber<O, U>>
-  for MapReturnRefOp<S, M, B>
-where
-  S: RawSubscribable<B, Err, Subscriber<MapReturnRefObserver<O, M>, U>>,
-  M: FnMut(B) -> Item,
-{
-  type Unsub = S::Unsub;
-  fn raw_subscribe(self, subscriber: Subscriber<O, U>) -> Self::Unsub {
-    let map = self.func;
-    self.source.raw_subscribe(Subscriber {
-      observer: MapReturnRefObserver {
-        observer: subscriber.observer,
-        map,
-      },
-      subscription: subscriber.subscription,
-    })
-  }
-}
-
-pub struct MapReturnRefObserver<O, M> {
-  observer: O,
-  map: M,
-}
-
-impl<Item, Err, O, M, B> Observer<Item, Err> for MapReturnRefObserver<O, M>
-where
-  O: Observer<B, Err>,
-  M: FnMut(Item) -> B,
-{
-  fn next(&mut self, value: Item) { self.observer.next((self.map)(value)) }
-
-  #[inline(always)]
-  fn error(&mut self, err: Err) { self.observer.error(err); }
-
-  #[inline(always)]
-  fn complete(&mut self) { self.observer.complete(); }
-}
-
-impl<S, M, B> Fork for MapReturnRefOp<S, M, B>
-where
-  S: Fork,
-  M: Clone,
-{
-  type Output = MapReturnRefOp<S::Output, M, B>;
-  fn fork(&self) -> Self::Output {
-    MapReturnRefOp {
-      source: self.source.fork(),
-      func: self.func.clone(),
-      _p: PhantomData,
-    }
-  }
-}
-
-impl<O, M> IntoShared for MapReturnRefObserver<O, M>
-where
-  O: IntoShared,
-  M: Send + Sync + 'static,
-{
-  type Shared = MapReturnRefObserver<O::Shared, M>;
-  fn to_shared(self) -> Self::Shared {
-    MapReturnRefObserver {
-      observer: self.observer.to_shared(),
-      map: self.map,
-    }
-  }
-}
-
-impl<S, M, B> IntoShared for MapReturnRefOp<S, M, B>
-where
-  S: IntoShared,
-  M: Send + Sync + 'static,
-  B: Send + Sync + 'static,
-{
-  type Shared = SharedOp<MapReturnRefOp<S::Shared, M, B>>;
-  fn to_shared(self) -> Self::Shared {
-    SharedOp(MapReturnRefOp {
-      source: self.source.to_shared(),
-      func: self.func,
-      _p: PhantomData,
-    })
-  }
-}
-
 #[cfg(test)]
 mod test {
   use crate::{ops::Map, prelude::*};
@@ -229,9 +126,7 @@ mod test {
   fn reference_lifetime_should_work() {
     let mut i = 0;
 
-    observable::of(100)
-      .map_return_ref(|v| v)
-      .subscribe(|v| i += v);
+    observable::of(100).map(|v| v).subscribe(|v| i += v);
     assert_eq!(i, 100);
   }
 
@@ -258,9 +153,9 @@ mod test {
       .subscribe(|_| {});
 
     // ref to ref can fork
-    let m = observable::from_iter(0..100).map_return_ref(|v| v);
+    let m = observable::of(&1).map(|v| v);
     m.fork()
-      .map_return_ref(|v| v)
+      .map(|v| v)
       .fork()
       .to_shared()
       .fork()
