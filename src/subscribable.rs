b23f4e496f40a163826361655db7398e92895728
adoo <Adoo@outlook.com>
2019-12-19 23:32:20 +0800

    refactor: Add three single mehtod traits `ObserverComplete`, ObserverError`, `ObserverNext` to help

--- src/subscribable.rs
+++ src/subscribable.rs
@@ -12,12 +12,43 @@ pub use subscribable_comp::*;
 use std::cell::RefCell;
 use std::rc::Rc;
 
+/// An Observer is a consumer of values delivered by an Observable. One for each
+/// type of notification delivered by the Observable: `next`, `error`,
+/// and `complete`.
+///
 /// `Item` the type of the elements being emitted.
 /// `Err`the type of the error may propagating.
 pub trait Observer<Item, Err> {
   fn next(&mut self, value: Item);
   fn error(&mut self, err: Err);
   fn complete(&mut self);
+}
+
+/// ObserverNext can consume the items delivered by an Observable.
+pub trait ObserverNext<Item> {
+  fn next(&mut self, value: Item);
+}
+
+/// ObserverNext can consume the error delivered by an Observable.
+pub trait ObserverError<Err> {
+  fn error(&mut self, err: Err);
+}
+
+/// ObserverNext can consume the complete notify delivered by an Observable.
+pub trait ObserverComplete {
+  fn complete(&mut self);
+}
+
+impl<Item, Err, T> Observer<Item, Err> for T
+where
+  T: ObserverNext<Item> + ObserverError<Err> + ObserverComplete,
+{
+  #[inline(always)]
+  fn next(&mut self, value: Item) { self.next(value); }
+  #[inline(always)]
+  fn error(&mut self, err: Err) { self.error(err); }
+  #[inline(always)]
+  fn complete(&mut self) { self.complete() }
 }
 
 pub trait IntoShared {
@@ -43,24 +74,11 @@ macro observer_impl_proxy_directly($item: ty, $err: ty) {
 impl<'a, Item, Err> Observer<Item, Err> for Box<dyn Observer<Item, Err> + 'a> {
   observer_impl_proxy_directly!(Item, Err);
 }
-impl<'a, Item, Err> Observer<&mut Item, Err>
-  for Box<dyn for<'r> Observer<&'r mut Item, Err> + 'a>
-{
-  observer_impl_proxy_directly!(&mut Item, Err);
-}
-impl<'a, Item, Err> Observer<Item, &mut Err>
-  for Box<dyn for<'r> Observer<Item, &'r mut Err> + 'a>
-{
-  observer_impl_proxy_directly!(Item, &mut Err);
-}
-impl<'a, Item, Err> Observer<&mut Item, &mut Err>
-  for Box<dyn for<'r> Observer<&'r mut Item, &'r mut Err> + 'a>
-{
-  observer_impl_proxy_directly!(&mut Item, &mut Err);
-}
+
 impl<'a, Item, Err> Observer<Item, Err> for Box<dyn Publisher<Item, Err> + 'a> {
   observer_impl_proxy_directly!(Item, Err);
 }
+
 impl<'a, Item, Err> Observer<&mut Item, Err>
   for Box<dyn for<'r> Publisher<&'r mut Item, Err> + 'a>
 {
