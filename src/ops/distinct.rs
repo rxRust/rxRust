bf55a9a987a67c4cea745018407e5a7d15e80ea1
wufniks <wufniks@gmail.com>
2021-12-13 17:27:44 +0900

    feat: 🎸 Implement distinct_key operator

--- src/ops/distinct.rs
+++ src/ops/distinct.rs
@@ -51,6 +51,63 @@ where
 }
 
 #[derive(Clone)]
+pub struct DistinctKeyOp<S, F> {
+  pub(crate) source: S,
+  pub(crate) key: F,
+}
+
+impl<S: Observable, F> Observable for DistinctKeyOp<S, F> {
+  type Item = S::Item;
+  type Err = S::Err;
+}
+
+impl_local_shared_both! {
+  impl<S, F, K> DistinctKeyOp<S, F>;
+  type Unsub = S::Unsub;
+  macro method($self: ident, $observer: ident, $ctx: ident) {
+    $self.source.actual_subscribe(DistinctKeyObserver {
+      observer: $observer,
+      key: $self.key,
+      seen: HashSet::new(),
+    })
+  }
+  where
+    S: @ctx::Observable,
+    F: Fn(&S::Item) -> K
+      @ctx::local_only(+ 'o)
+      @ctx::shared_only(+ Send + Sync + 'static),
+    K: Eq + Hash + Clone
+      @ctx::local_only(+ 'o)
+      @ctx::shared_only(+ Send + Sync + 'static),
+}
+struct DistinctKeyObserver<O, F, K> {
+  observer: O,
+  key: F,
+  seen: HashSet<K>,
+}
+
+impl<O, F, K, Item, Err> Observer for DistinctKeyObserver<O, F, K>
+where
+  O: Observer<Item = Item, Err = Err>,
+  K: Hash + Eq + Clone,
+  F: Fn(&Item) -> K,
+{
+  type Item = Item;
+  type Err = Err;
+  fn next(&mut self, value: Self::Item) {
+    let key = (self.key)(&value);
+    if !self.seen.contains(&key) {
+      self.seen.insert(key);
+      self.observer.next(value);
+    }
+  }
+
+  fn error(&mut self, err: Self::Err) { self.observer.error(err) }
+
+  fn complete(&mut self) { self.observer.complete() }
+}
+
+#[derive(Clone)]
 pub struct DistinctUntilChangedOp<S> {
   pub(crate) source: S,
 }
@@ -232,5 +289,19 @@ mod tests {
     .subscribe(move |v| x.borrow_mut().push(v))
     .unsubscribe();
     assert_eq!(&*x_c.borrow(), &[(1, 2), (2, 2), (1, 1), (2, 2), (3, 2)]);
+  }
+
+  #[test]
+  fn distinct_key() {
+    let x = Rc::new(RefCell::new(vec![]));
+    let x_c = x.clone();
+    observable::from_iter(
+      vec![(1, 2), (2, 2), (2, 1), (1, 1), (2, 2), (3, 2)].into_iter(),
+    )
+    .map(|v| v)
+    .distinct_key(|tup: &(i32, i32)| tup.0)
+    .subscribe(move |v| x.borrow_mut().push(v))
+    .unsubscribe();
+    assert_eq!(&*x_c.borrow(), &[(1, 2), (2, 2), (3, 2)]);
   }
 }
