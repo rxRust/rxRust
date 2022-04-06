25f7770024bf4ed979ac193bbed72b8d60ba1d31
신희제(Heejae Shin) <heejae.shin@momenti.tv>
2022-04-06 09:44:53 +0900

    fix: don't refer external library directly

--- src/observable/interval.rs
+++ src/observable/interval.rs
@@ -1,6 +1,7 @@
-use crate::{impl_helper::*, impl_local_shared_both, prelude::*};
+use crate::{
+  impl_helper::*, impl_local_shared_both, prelude::*, scheduler::Instant,
+};
 
-use fluvio_wasm_timer::Instant;
 use std::time::Duration;
 
 /// Creates an observable which will fire at `dur` time into the future,
