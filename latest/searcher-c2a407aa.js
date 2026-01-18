e4b650ba43405269fdc24ee1098595e8024708ee
wjian23 <wjian23@users.noreply.github.com>
2026-01-18 13:58:34 +0000

    deploy: fdaa83eafcf226454f680c630f1f7f406e3f0ba3

--- latest/searcher-c2a407aa.js
+++ latest/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2afe6447.js',
+                path_to_root + 'searchindex-c6d0df11.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
